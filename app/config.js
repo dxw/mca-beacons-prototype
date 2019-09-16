eb8f1e6174798a99eee85705bf78748a151ef6f1
Gaz Aston <gaz@dxw.com>
2019-09-16 11:03:08 +0100

    adds index page and renames service

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Register a beacon',
+  serviceName: 'Maritime & Coastguard Agency',
 
   // Default port that prototype runs on
   port: '3000',
